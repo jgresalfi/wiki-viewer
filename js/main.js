b9da616fe3ef658d13e78c6be37b2681e5dfcd1a
Jason Gresalfi <jason.gresalfi@gmail.com>
2016-11-03 21:56:50 -0500

    Added comments to JS and started to build out AJAX call for search results

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function() {
+
+    //Event listener to reveal search field on magnifying glass click
     var clicker = document.querySelector("#clicker"),
         searchBox = document.querySelector("#search");
 
     function searchReveal() {
-        console.log("searchReveal function firing!");
         searchBox.classList.toggle("reveal");
     }
 
     clicker.onclick = searchReveal;
+    //End search field click event code
+
+    //AJAX call and response callback
+    var searchTxt = document.querySelector("#search");
+
+    function getResults() {
+        console.log(searchTxt.value);
+        //AJAX code goes here...
+    }
+
+    searchTxt.onchange = getResults;
+
+
+
 })
