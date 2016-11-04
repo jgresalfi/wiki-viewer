61ffaa6e3338269aa3ca8f6c87d555d7521784f5
Jason Gresalfi <jason.gresalfi@gmail.com>
2016-11-04 14:38:46 -0500

    Working on ajax request and adding to dom stuff

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     //Event listener to reveal search field on magnifying glass click
-    var clicker = document.querySelector("#clicker"),
-        searchBox = document.querySelector("#search");
+    var clicker = document.getElementById("clicker"),
+        searchBox = document.getElementById("search");
 
     function searchReveal() {
         searchBox.classList.toggle("reveal");
@@ -13,15 +13,42 @@ document.addEventListener("DOMContentLoaded", function() {
     clicker.onclick = searchReveal;
     //End search field click event code
 
-    //AJAX call and response callback
-    var searchTxt = document.querySelector("#search");
+    //AJAX call and response callback function
+    var searchTxt = document.getElementById("search"),
+        ajaxURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Albert%20Einstein&format=json&utf8=";
 
-    function getResults() {
-        console.log(searchTxt.value);
-        //AJAX code goes here...
+    function getResponse(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+        xhr.open('GET', url);
+        xhr.onload = function() {
+            if (this.status >= 200 && this.status < 400) {
+                console.log(this.status);
+                callback(this.response);
+            } else {
+                console.log(this.error);
+            }
+        }; //End xhr.onload
+        xhr.setRequestHeader("cache-control", "no-cache");
+        xhr.setRequestHeader("postman-token", "5cef329f-7549-4ee8-c727-26fbff8c8d6e");
+        xhr.send();
+    } //End getResponse
+
+    function responseWrite(data) {
+        var resultDiv = document.getElementById("result"),
+            artFrag = document.createDocumentFragment(),
+            artTitle = document.createElement("h3"),
+            artSnip = document.createElement("p");
+        for (var i = 0; i < data.search.length; i++) {
+            artTitle.innerHTML = data.search[i].title;
+            artSnip.innerHTML = data.search[i].snippet;
+            artFrag.append(artTitle, artSnip);
+        }
+        resultDiv.append(artFrag);
     }
 
-    searchTxt.onchange = getResults;
+    //Fires getResponse() and responseWrite() to get data and write it to page
+    searchTxt.onchange = getResponse(ajaxURL, responseWrite);
 
 
 
